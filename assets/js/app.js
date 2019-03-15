adaea924ca7ed802fae6a290d97beb0d2865bf5e
Robin Gagnon <robin.gagnon@cortexmedia.ca>
2019-03-15 11:39:35 -0400

    Added "send" button

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,14 @@
-// We need to import the CSS so that webpack will load it.
-// The MiniCssExtractPlugin is used to separate it out into
-// its own CSS file.
 import css from "../css/app.css"
-
-// webpack automatically bundles all modules in your
-// entry points. Those entry points can be configured
-// in "webpack.config.js".
-//
-// Import dependencies
-//
 import "phoenix_html"
-
-// Import local files
-//
-// Local files can be imported directly using relative paths, for example:
 import socket from "./socket"
 
 const channel = socket.channel('room:lobby', {});
 
-channel.on('shout', payload => {
-  const msgDisplay = document.createElement("div");
-  const name = payload.name || 'guest';
-  msgDisplay.innerHTML = '<b>' + name + '</b>: ' + payload.message;
+channel.on('shout', data => {
+  const msgDisplay = document.createElement("span");
+  msgDisplay.classList.add("msg");
+  const name = data.name || 'guest';
+  msgDisplay.innerHTML = '<b>' + name + '</b>: ' + data.message;
   msgContainer.appendChild(msgDisplay);                    
 });
 
@@ -29,15 +16,24 @@ channel.join();
 
 const msgContainer = document.getElementById('msg-list');
 const name = document.getElementById('name');
-const msg = document.getElementById('msg');
+const msg = document.getElementById('msg-text');
+const btn = document.getElementById('send-btn')
 
-// "listen" for the [Enter] keypress event to send a message:
-msg.addEventListener('keypress', function (event) {
-  if (event.keyCode == 13 && msg.value.length > 0) { // don't sent empty msg.
-    channel.push('shout', { // send the message to the server on "shout" channel
-      name: name.value,     // get value of "name" of person sending the message
-      message: msg.value    // get message text (value) from msg input field.
+const shout = () => {
+  if (msg.value.length > 0) {
+    channel.push('shout', {
+      name: name.value,
+      message: msg.value
     });
-    msg.value = '';         // reset the message input field for next message.
+    msg.value = '';
+  }
+}
+
+msg.addEventListener('keypress', event => {
+  if (event.keyCode == 13) {
+    shout();
   }
 });
+
+btn.addEventListener('mouseup', () => shout());
+
