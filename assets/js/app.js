5d49863c502176f041100851cc1cacb8556802c9
Robin Gagnon <robin.gagnon@cortexmedia.ca>
2019-03-18 13:30:07 -0400

    Fix method use for best practice
    node.innerText is closer semantically than node.innerHTML

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,5 @@ import "phoenix_html"
 import socket from "./socket"
 import Room from "./room"
 
-const roomName = document.getElementById('room-id').innerHTML;
+const roomName = document.getElementById('room-id').innerText;
 const room = new Room(socket, roomName);
-
-
