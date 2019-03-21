36d4026327365763155c346c59278b9ec85b9b8d
Robin Gagnon <robin.gagnon@cortexmedia.ca>
2019-03-21 10:10:21 -0400

    Handle non existing user name when signing in

--- lib/hub_web/controllers/session_controller.ex
+++ lib/hub_web/controllers/session_controller.ex
@@ -3,16 +3,20 @@ defmodule HubWeb.SessionController do
   alias Hub.Accounts
 
   def new(conn, _params) do
-    render(conn, "new.html")
+    render(conn, "new.html", %{error: ""})
   end
 
   def create(conn, %{"session" => auth_params}) do
-    user = Accounts.get_by_username(auth_params["username"])
+    case Accounts.get_by_username(auth_params["username"]) do
+      nil ->
+        render(conn, "new.html", %{error: "Username doesn't exist"})
 
-    conn
-    |> put_session(:current_user_id, user.id)
-    |> put_flash(:info, "Signed in successfully.")
-    |> redirect(to: Routes.page_path(conn, :index))
+      user ->
+        conn
+        |> put_session(:current_user_id, user.id)
+        |> put_flash(:info, "Signed in successfully.")
+        |> redirect(to: Routes.page_path(conn, :index))
+    end
   end
 
   def delete(conn, _params) do
