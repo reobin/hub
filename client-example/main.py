35235cfc6b0d149c850b4f4ce56eded767fef371
Robin Gagnon <robin.gagnon@cortexmedia.ca>
2019-04-05 11:12:57 -0400

    Connect to the web socket and join a chat room (python client)

--- client-example/main.py
+++ client-example/main.py
@@ -1,6 +1,28 @@
-def main():
-    print("Hello, world!")
+#!/usr/bin/env python
+
+# WS client example
+
+import asyncio
+import websockets
+import json
+
+URL = "wss://guarded-fortress-70241.herokuapp.com/socket/websocket"
 
 
-if __name__ == "__main__":
-    main()
+async def join_a_room():
+    async with websockets.connect(URL) as websocket:
+        data = dict(
+            topic="room:lobby",
+            event="phx_join",
+            payload={"username": "reobin"},
+            ref=None,
+        )
+        await websocket.send(json.dumps(data))
+        print(f"> {data}")
+
+        recv = await websocket.recv()
+        print(f"< {recv}")
+
+
+asyncio.get_event_loop().run_until_complete(join_a_room())
+
