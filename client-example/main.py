dbec1471e15e23dea72e3dd475262b4e94c8984f
Robin Gagnon <robin.gagnon@cortexmedia.ca>
2019-04-05 13:22:06 -0400

    Receive entering messages asynchronously on python client

--- client-example/main.py
+++ client-example/main.py
@@ -5,29 +5,71 @@
 import asyncio
 import websockets
 import json
+import sys
 
 URL = "wss://guarded-fortress-70241.herokuapp.com/socket/websocket"
 
 
-async def join_a_room():
-    async with websockets.connect(URL) as websocket:
-        data = dict(
-            topic="room:lobby",
-            event="phx_join",
-            payload={"username": "reobin"},
-            ref=None,
-        )
-        await websocket.send(json.dumps(data))
-        print(f"> {data}")
+class Prompt:
+    def __init__(self, loop=None):
+        self.loop = loop or asyncio.get_event_loop()
+        self.q = asyncio.Queue(loop=self.loop)
+        self.loop.add_reader(sys.stdin, self.on_input)
 
-        response = await websocket.recv()
-        print(f"< {response}")
+    def on_input(self):
+        asyncio.ensure_future(self.q.put(sys.stdin.readline()), loop=self.loop)
 
-        while True:
-            message = json.loads(await websocket.recv())
-            if message["event"] == "shout":
-                print(f"< {message['payload']['body']}")
+    async def __call__(self, msg=None, **kwargs):
+        if msg:
+            print(msg, **kwargs)
+        return (await self.q.get()).rstrip("\n")
 
 
-asyncio.get_event_loop().run_until_complete(join_a_room())
+async def send_messages(connection, room_name):
+    prompt = Prompt()
+    while True:
+        message_body = await prompt()
+        message = {
+            "topic": f"room:{room_name}",
+            "event": "shout",
+            "payload": {"body": message_body, "channel": room_name, "name": "reobin"},
+            "ref": None,
+        }
+
+        await connection.send(json.dumps(message))
+
+
+async def create_connection(room_name):
+    connection = await websockets.connect(URL)
+    data = {
+        "topic": f"room:{room_name}",
+        "event": "phx_join",
+        "payload": {"username": "reobin"},
+        "ref": None,
+    }
+    await connection.send(json.dumps(data))
+    print(f"> {data}")
+    return connection
+
+
+async def join_a_room(websocket):
+
+    response = await websocket.recv()
+    print(f"< {response}")
+
+    async for message_body in websocket:
+        message = json.loads(message_body)
+
+        if message["event"] == "shout":
+            print(f"< {message['payload']['body']}")
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    room_name = sys.argv[1]
+    connection = loop.run_until_complete(create_connection(room_name))
+
+    asyncio.ensure_future(join_a_room(connection))
+    asyncio.ensure_future(send_messages(connection, room_name))
+    loop.run_forever()
 
